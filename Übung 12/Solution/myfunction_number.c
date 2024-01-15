dc664a662ad4d9c535f269d263d293c892758ea6
Dominik <dominik.gorgosch@informatik.tu-chemnitz.de>
2024-01-15 09:03:05 +0100

    Added solution.

---
+++ Übung 12/Solution/myfunction_number.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "myfunction.h"
+
+void print_number(int number) {
+    printf("Number: %d\n", number);
+}
---
+++ Übung 12/Solution/myfunction_string.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "myfunction.h"
+
+void print_hello() {
+    printf("Hallo Welt!!!\n");
+}
